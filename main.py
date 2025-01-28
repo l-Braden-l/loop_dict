491b2b71d283b86d9dcad621ccaf7df5dcf61c2e
braden <leachbr787@northwested.org>
2025-01-28 13:04:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#Braden Leach
+#January 28 2024
+#Looping Dictionary
 
+
+
+
+#---Looping Dictionary---#
+mounatains = {
+        'Andes': 6962,
+        'Rockies': 4401,
+        'Urals': 1895,
+        'Alps': 4807,
+        'Himalayas': 8848
+}
+
+for key,value in mounatains.items():
+    print(f'The {key} have a height of {value} meters tall!')
